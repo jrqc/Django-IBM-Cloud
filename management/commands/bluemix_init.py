eccd4cae5b91ac10e533213bef3ff2fd5eb50b28
jrqc <gurbuz@oc5613037512.ibm.com>
2018-02-19 13:17:55 +0300

    fix

--- management/commands/bluemix_init.py
+++ management/commands/bluemix_init.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
         parser.add_argument('application_name', type=str)
 
     def handle(self, *args, **options):
+        check = "Project successfully configured for: %s"
         application_name = options['application_name']
         base = settings.BASE_DIR
         python_version = sys.version.split(' ')[0]
@@ -20,20 +21,24 @@ class Command(BaseCommand):
             f = open(base + "/Procfile", "w")
             f.write("web: python manage.py migrate && gunicorn {}.wsgi --log-file -".format(project_name))
             f.close()
-        except IOError:
-            print "IO"
+        except Exception as e:
+            check = "Exception at creating Procfile :"
+            print "failed to create Procfile {}".format(e)
         try:
             m = open(base + "/manifest.yml", "w")
             m.write(" applications: \n  - name: {} ".format(application_name))
             m.close()
         except Exception as e:
-            print e
+            check = "Exception at creating manifest.yml"
+            print "failed to create manifest.yml - {}".format(e)
 
         try:
             v = open(base + "/runtime.txt", "w")
             v.write("python-{}".format(python_version))
             v.close()
-        except Exception as E:
-            print E
+        except Exception as e:
+            check = "Exception at creating runtime.txt"
+            print "failed to create runtime.txt - {}".format(e)
 
-        self.stdout.write('Project successfully configured for: %s \n' % str(application_name))
+        self.stdout.write(check % str(application_name))
+
